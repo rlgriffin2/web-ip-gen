b8c39df82c7bdf92834e936610c85060186ca306
rlgriffin2 <rlgriffin2@wisc.edu>
2019-11-17 13:50:40 -0600

    new scripts and more automatic

---
+++ findIP.py
@@ -0,0 +1,12 @@
+import subprocess
+#firstIP = subprocess.run(['hostname', '-I'], stdout=subprocess.PIPE).stdout.decode()
+#p1 = subprocess.run(['hostname', '-I'], stdout=subprocess.PIPE)
+#oldip = p1.stdout.decode()
+p2 = subprocess.run(['hostname', '-I'], stdout=subprocess.PIPE)
+newip = p2.stdout.decode()
+#if(oldip~=newip):
+#    publish(newip)
+publish(newip)
+
+def publish(ipaddr):
+    subprocess.call(['/home/riley/scripts/publiship'])
