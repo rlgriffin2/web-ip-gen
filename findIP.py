27898b7fe70449171bd663faea36271f81d783bd
rlgriffin2 <rlgriffin2@wisc.edu>
2019-11-17 14:28:09 -0600

    auto-updated ip

--- findIP.py
+++ findIP.py
@@ -1,9 +1,14 @@
 import subprocess
+import sys
 #firstIP = subprocess.run(['hostname', '-I'], stdout=subprocess.PIPE).stdout.decode()
 #p1 = subprocess.run(['hostname', '-I'], stdout=subprocess.PIPE)
 #oldip = p1.stdout.decode()
-p2 = subprocess.run(['hostname', '-I'], stdout=subprocess.PIPE)
-newip = p2.stdout.decode()
-#if(oldip~=newip):
-#    publish(newip)
-subprocess.call(['./publishIP', '-$newip'])
+
+
+def publish(username, password):
+    p2 = subprocess.run(['hostname', '-I'], stdout=subprocess.PIPE)
+    newip = p2.stdout.decode()
+    subprocess.call(['./publishIP', '-$newip', '-$username', '-$password'])
+
+if __name__ == '__main__':
+    publish((sys.argv[1]),(sys.argv[2]))
